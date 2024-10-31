6fa217b07126ad7b9e8120ab709504eb5b240fa4
Si <tlgyrjs@naver.com>
2024-10-31 17:51:46 +0900

    initial commit

---
+++ src/test/java/com/spring/orderservice_back202410/OrderServiceBack202410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.orderservice_back202410;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceBack202410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
